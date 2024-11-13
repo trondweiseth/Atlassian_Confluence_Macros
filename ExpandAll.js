0e81c9426dc2feab45b2975d3603c0343e29add6
W3i537h <maatro_1001@hotmail.com>
2024-11-13 17:42:48 +0100

    Update ExpandAll.js

--- ExpandAll.js
+++ ExpandAll.js
@@ -7,25 +7,32 @@
 ## @noparams
 
 <p align="center">
-    <button name="toggleAll" id="toggleAll" type="button" class="aui-button" aria-pressed="false">Expand/Collapse All</button>
+    <button name="toggleAll" id="toggleAll" type="button" class="aui-button" aria-pressed="false">Expand All</button>
 </p>
 
 <script type="text/javascript">
 AJS.toInit(function () {
     AJS.$('#toggleAll').click(function () {
-        // Find all expand/collapse controls based on the new code block elements
+        var button = AJS.$(this);  // Get the button element
         var expanded = AJS.$('.expand-control-icon.icon').filter('.expanded');
         var unexpanded = AJS.$('.expand-control-icon.icon').not('.expanded');
 
-        if (expanded.length > unexpanded.length) {
-            // If more are expanded, collapse them
-            expanded.each(function () {
-                AJS.$(this).closest("div").find('.expand-control-text').trigger("click");
+        // Expand All Logic
+        if (button.text() === "Expand All") {
+            // Expand all macros
+            jQuery(".expand-control").each(function () {
+                var $expander = jQuery(this);
+                var $expanderIcon = jQuery(".expand-control-icon", $expander);
+                var $expanderContent = jQuery(".expand-content", $expander.closest(".expand-container")).first();
+
+                $expanderContent.show();
+                $expanderContent.animate({ opacity: 1 });
             });
 
-            // Update the aria-pressed state
-            AJS.$("#toggleAll").attr("aria-pressed", "false");
-        } else {
+            // Change button text to "Collapse All"
+            button.text("Collapse All");
+            button.attr("aria-pressed", "true");  // Mark as expanded
+
             // If more are collapsed, expand them
             unexpanded.each(function () {
                 AJS.$(this).closest("div").find('.expand-control-text').trigger("click");
@@ -33,25 +40,32 @@ AJS.toInit(function () {
 
             // Update the aria-pressed state
             AJS.$("#toggleAll").attr("aria-pressed", "true");
+        } 
+        // Collapse All Logic
+        else if (button.text() === "Collapse All") {
+            // Collapse all macros
+            jQuery(".expand-control").each(function () {
+                var $expander = jQuery(this);
+                var $expanderIcon = jQuery(".expand-control-icon", $expander);
+                var $expanderContent = jQuery(".expand-content", $expander.closest(".expand-container")).first();
+
+                // Ensure the content is hidden (collapse it)
+                $expanderContent.hide();  // Hide content
+                $expanderIcon.removeClass("expanded");  // Remove "expanded" class
+                $expanderContent.addClass("expand-hidden");  // Add hidden class
+            });
+
+            // Change button text to "Expand All"
+            button.text("Expand All");
+            button.attr("aria-pressed", "false");  // Mark as collapsed
+
+            // If more are expanded, collapse them
+            expanded.each(function () {
+                AJS.$(this).closest("div").find('.expand-control-text').trigger("click");
+            });
+
+            AJS.$("#toggleAll").attr("aria-pressed", "false");
         }
-        jQuery(".expand-control").each(function () {
-            var $expander = jQuery(this);
-            var $expanderIcon = jQuery(".expand-control-icon", $expander);
-            var $expanderContent = jQuery(".expand-content", $expander.closest(".expand-container")).first();
-
-            // Toggle expansion/collapse
-            if ($expanderContent.hasClass("expand-hidden")) {
-                $expanderContent.show();
-                $expanderContent.animate({ opacity: 1 });
-            } else {
-                $expanderContent.hide();
-                $expanderContent.animate({ opacity: 0 });
-            }
-
-            // Toggle the relevant classes
-            $expanderContent.toggleClass("expand-hidden");
-            $expanderIcon.toggleClass("expanded");
-        });
     });
 });
 </script>
